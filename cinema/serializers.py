e8195a382ef710231b06a8e0b2e851256b094299
ira <irynaira@gmail.com>
2024-10-19 00:57:30 +0300

    for AI v3

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -26,13 +26,15 @@ class CinemaHallSerializer(serializers.ModelSerializer):
 
 
 class MovieSerializer(serializers.ModelSerializer):
-    genres = serializers.ListField(
-        child=serializers.IntegerField(),
-        required=True)
+    genres = serializers.StringRelatedField(many=True)
+    actors = serializers.SerializerMethodField()
 
     class Meta:
         model = Movie
-        fields = ["id", "title", "description", "duration", "genres", "actors"]
+        fields = ['id', 'title', 'description', 'duration', 'genres', 'actors']
+
+    def get_actors(self, obj):
+        return [f"{actor.first_name} {actor.last_name}" for actor in obj.actors.all()]
 
 
 class MovieSessionSerializer(serializers.ModelSerializer):
